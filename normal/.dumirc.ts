25f3a31d521282cad114edcf817f93225cb886ac
miracles1919 <516571350@qq.com>
2022-12-12 15:10:55 +0800

    init

---
+++ normal/.dumirc.ts
@@ -0,0 +1,12 @@
+export default {
+  locales: [
+    { id: 'zh-CN', name: '中文' },
+    { id: 'en-US', name: 'EN' },
+  ],
+  themeConfig: { name: '示例' },
+  mfsu: false,
+  apiParser: {},
+  resolve: { entryFile: './src/index.ts' },
+  base: '/dumi-exapmles/',
+  publicPath: '/dumi-exapmles/',
+};
